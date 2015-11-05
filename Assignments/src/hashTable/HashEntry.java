5f2b60edb6626e8e9ae4a5b0e256cea12a43d159
Dave Luk <dluk960510@gmail.com>
2015-11-04 20:40:38 -0800

    Added Reference: HashTable

---
+++ Assignments/src/hashTable/HashEntry.java
@@ -0,0 +1,46 @@
+package hashTable;
+
+public class HashEntry<K, E>
+{
+
+	private K	key;
+	private E	element;
+
+	private HashEntry<K, E> next;
+
+	public HashEntry(K key, E element)
+	{
+		this.key = key;
+		this.element = element;
+	}
+
+	public K getKey()
+	{
+		return key;
+	}
+
+	public void setKey(K key)
+	{
+		this.key = key;
+	}
+
+	public E getElement()
+	{
+		return element;
+	}
+
+	public void setElement(E element)
+	{
+		this.element = element;
+	}
+
+	public HashEntry<K, E> getNext()
+	{
+		return next;
+	}
+
+	public void setNext(HashEntry<K, E> next)
+	{
+		this.next = next;
+	}
+}
---
+++ Assignments/src/hashTable/Hashable.java
@@ -0,0 +1,8 @@
+package hashTable;
+
+public interface Hashable<K,E>
+{
+	public int hashCode();
+	
+	public boolean equals();
+}
